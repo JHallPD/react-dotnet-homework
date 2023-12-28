3bebbe8e10165833d59545b4c260480c2fd6f669
Jeff <jeffahall94@gmail.com>
2023-12-27 19:41:26 -0500

    Backend Testing Updates
    The backend tests require some rework to properly cover db crud operations which cypress e2e testing covers already.
    
    I have removed an email assert for the first test as its only for name and id, I have also updated the second test to include the new fields for contact
    
    full test coverage will be provided with cypress to avoid unnecessary reworking

--- backend/Contacts.Tests/ContactTests.cs
+++ backend/Contacts.Tests/ContactTests.cs
@@ -1,25 +1,34 @@
-namespace Contacts.Tests;
+using Moq;
 
-public class ContactTests
+
+namespace Contacts.Tests
 {
-    [Fact]
-    public void Constructor_Sets_Name_And_Id()
+    // the way the backend is setup there is no contact controller or repository
+    // cypress covers most of this with e2e testing so there is no benefit to fixing this to cover create, read, update and delete
+    public class ContactTests
     {
-        var contact = new Contact("Test Name", 1);
-        Assert.Equal("Test Name", contact.Name);
-        Assert.Equal(1, contact.Id);
-        Assert.Equal("test_name@test.com", contact.Email);
-    }
 
-    [Fact]
-    public void Constructor_Sets_All_Properties()
-    {
-        var contact = new Contact("Test Name", "test@test.com", "1234567890", "avatar.png", "@test", "Test notes");
-        Assert.Equal("Test Name", contact.Name);
-        Assert.Equal("test@test.com", contact.Email);
-        Assert.Equal("1234567890", contact.Phone);
-        Assert.Equal("avatar.png", contact.Avatar);
-        Assert.Equal("@test", contact.Twitter);
-        Assert.Equal("Test notes", contact.Notes);
+        [Fact]
+        public void Constructor_Sets_Name_And_Id()
+        {
+            var contact = new Contact("Test Name", 1);
+            Assert.Equal("Test Name", contact.Name);
+            Assert.Equal(1, contact.Id);
+        }
+
+        [Fact]
+        public void Constructor_Sets_All_Properties()
+        {
+            var contact = new Contact("Test Name", "test@test.com", "1234567890", "avatar.png", "@test", "Test notes", DateTime.Parse("0001-01-01 00:00:00.0000000"), DateTime.Parse("0001-01-01 00:00:00.0000000"));
+            Assert.Equal("Test Name", contact.Name);
+            Assert.Equal("test@test.com", contact.Email);
+            Assert.Equal("1234567890", contact.Phone);
+            Assert.Equal("avatar.png", contact.Avatar);
+            Assert.Equal("@test", contact.Twitter);
+            Assert.Equal("Test notes", contact.Notes);
+            Assert.Equal(DateTime.Parse("0001-01-01 00:00:00.0000000"), contact.CreatedAt);
+            Assert.Equal(DateTime.Parse("0001-01-01 00:00:00.0000000"), contact.UpdatedAt);
+        }
     }
 }
+
